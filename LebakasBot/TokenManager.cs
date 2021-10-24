2919fa25710079834fe7a9c2a8123d220e916ad9
Harpunen Harry <harald.tributsch@outlook.com>
2021-10-24 15:46:36 +0200

    Update TokenManager.cs

--- LebakasBot/TokenManager.cs
+++ LebakasBot/TokenManager.cs
@@ -4,24 +4,11 @@ namespace LebakasBot
 {
     public class TokenManager
     {
-        private const string cTokenPathVar = "TOKEN_PATH";
         public string Token { get; }
 
         public TokenManager(string path = "token.txt")
         {
-            string pathToToken = System.Environment.GetEnvironmentVariable(cTokenPathVar);
-            if (string.IsNullOrWhiteSpace(pathToToken))
-            {
-                pathToToken = "";
-            }
-            else
-            {
-                if (!pathToToken.EndsWith('/'))
-                {
-                    pathToToken += '/';
-                }
-            }
-            Token = File.ReadAllText(pathToToken + path);
+            Token = File.ReadAllText(path);
         }
     }
 }
